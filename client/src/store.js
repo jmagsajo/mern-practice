963b5ea610e43720693b654a68d5efe2656250c7
Joseph Magsajo <jmagsajo@yondu.com>
2020-06-03 22:43:15 +0800

    update

--- client/src/store.js
+++ client/src/store.js
@@ -5,17 +5,16 @@ const initialState = {};
 const middleware = [thunk];
 
 // dev tools middleware
-const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+// const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
     
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    composeSetup
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
